e7beef79b8b33aa223673c81641be959f9510d21
Roopesh <roopesh.neo@gmail.com>
2022-10-05 15:02:36 +0100

    starter rest template

---
+++ src/main/java/com/learn/spring/rest/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.rest.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
